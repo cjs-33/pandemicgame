36d1ebdbd3c5a84e1b3c308a2db1856f2101ed30
CSimmons <cjsimmons89@gmail.com>
2020-12-12 23:49:51 -0500

    Adding better logic for rent being due. moving day counter to just above user input box. Adding a few more paths.

--- js/game.js
+++ js/game.js
@@ -11,6 +11,9 @@ var timesNotWornMask = 0;
 
 var wearingMask = false;
 
+var rentDueTomorrow = false;
+var rentDueDay = 0;
+
 const luck = Math.random();
 
 
@@ -27,7 +30,7 @@ $(function () {
         $("section#gametoolbar").show();
         updateMoney(money);
         updateCannedFoods(cannedFoods);
-        updateSoudough(sourdoughStarters);
+        updateSourdough(sourdoughStarters);
         updateMasks(masks);
 
         updateDay(1);
@@ -39,6 +42,10 @@ $(function () {
 
 $("#theInput").on('keyup', function (e) {
     if (e.key === 'Enter' || e.keyCode === 13) {
+        if (money < 0 || money == 0) {
+            alert("You can't pay your rent! You're being evicted because your state didn't put into place any temporary halts in residential evictions! You're now on the street, without a mask. Guess what happens next...");
+            getCovid();
+        }
         userInput = $(this).val();
         console.log(userInput);
 
@@ -80,13 +87,20 @@ $("#theInput").on('keyup', function (e) {
                 incrementDay();
 
                 break;
+            case "watch tv":
+            case "watch television":
+            case "tv":
+                appReact("You watch this awesome show called Attack on Titan and you get completely hooked by it. Some would say you 'lose' 2 days, but you really don't count them as lost at all. It is an amazing show and the final season starts in December. Can't wait.");
+                incrementDay();
+                incrementDay(); 
+                break;
             case "find job":
             case "find a job":
             case "look for a job":
                 if (luck > 0.9) {
                     appReact("Holy shit you actually found a job! You apply for it, and they call you back immediately! Wow! You answer the phone, and it's a recorded message saying the company is temporary closed. Oh well, back to Animal Crossing.");
                 } else {
-                    appReact("There aren't any of those. At least, not for you.");
+                    appReact("There aren't any jobs. At least, not for you.");
 
                 }
                 incrementDay();
@@ -111,13 +125,13 @@ $("#theInput").on('keyup', function (e) {
                 if (sourdoughStarters > 0) {
                     if (luck > 0.5) {
                         appReact("You somehow successfully don't kill your sourdough starter! You make some delicious bread. \"Shit, I could surivve in the 1400s no problem!\" you muse as you drizzle that gorgeous bread in warm butter and scarf it down. A gorgeous nap ensues. Evening came, and morning followed. Day " + day + " complete.");
-                        updateSoudough(sourdoughStarters - 1);
+                        updateSourdough(sourdoughStarters - 1);
                     } else if (luck > 0.2 && luck < 0.5) {
                         appReact("You literally have no idea what to do with this bread starter -- is sourdough even a bread? -- so you flail at the oven and nothing happens. ");
-                        updateSoudough(sourdoughStarters - 1);
+                        updateSourdough(sourdoughStarters - 1);
                     } else {
                         appReact("You tried at this thing for like, seriously 23 out of your 24 hours today. Apart from a few fires, where you learned how to use your fire extinguisher finally after you've had it in your broom closet for hte last 8 years wondering what it was half the time, this was an unmitigated disaster. You have no sourdough left, and it's probably for the best.");
-                        updateSoudough(0);
+                        updateSourdough(0);
                     }
                     incrementDay();
                 } else {
@@ -150,8 +164,12 @@ $("#theInput").on('keyup', function (e) {
             case "find masks":
             case "find more masks":
             case "order masks":
+            case "get masks":
+            case "get more masks":
+            case "buy masks":
+            case "buy mask":
                 if (luck > 0.2) {
-                    appReact("You go online looking for more masks. You find a case of 6 N-95 masks for $100 - \"fucking hoarders\", you mumble. <em>+ 5 masks</em>");
+                    appReact("You go online looking for more masks. You find a case of 5 N-95 masks for $100 - \"fucking hoarders\", you mumble. <em>+ 5 masks</em>");
                     updateMoney(money - 100);
                     updateMasks(masks + 5);
                 } else {
@@ -170,6 +188,8 @@ $("#theInput").on('keyup', function (e) {
                     if (!caughtCovid) {
                         appReact("You just got back from the outside world. It was generally fun, but something feels off --- oh shit, you weren't wearing your mask! Somehow you still feel ok, other than an insane sense of guilt and misfortune for having potentially exposed yourself and others to the effects of a deadly pandemic. Oh well! Time for Animal Crossing.");
                         incrementDay();
+                    } else {
+                        getCovid();
                     }
                 } else {
                     caughtCovid = checkCatch();
@@ -180,6 +200,12 @@ $("#theInput").on('keyup', function (e) {
                         incrementDay();
                     }
                 }
+                break;
+            case "sleep":
+            case "take nap":
+            case "take a nap":
+                appReact("You slept all day. Some would say it's safer than trying to bake your own bread.");
+                incrementDay();
                 break;
             case "get covid":
                 appReact("....Ok. You got covid.");
@@ -193,9 +219,24 @@ $("#theInput").on('keyup', function (e) {
 
         clearInput();
         clearPrompt();
+       
+
+
+        if (rentDueTomorrow && day == rentDueDay) {
+            updateMoney(money - 500);
+            rentDueTomorrow = false;
+        }
+    
         if (day % 5 == 0) {
             appPrompt("<strong>Your rent is due tomorrow! $500 will be debited from your account!</strong>");
+            rentDueTomorrow = true;
+            console.log("rent due: " + rentDueTomorrow);
+            rentDueDay = day + 1;
+
+        } else {
+            rentDueTomorrow = false;
         }
+
 
     }
 
@@ -244,19 +285,22 @@ function appPrompt(prompt) {
 }
 
 function updateCannedFoods(c) {
+    cannedFoods = c;
     $("#cannedFoods").val(c);
 
 }
 
 function updateMoney(m) {
+    money = m;
     $(".money").html("$" + m);
 
     $("#money").val(m);
 
 }
 
-function updateSoudough(s) {
+function updateSourdough(s) {
     $("#sourdoughStarter").val(s);
+    sourdoughStarters = s;
 }
 
 function updateMasks(m) {
